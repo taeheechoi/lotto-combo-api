ec99112c18c712c0cc49e71c7acff3fe83cb996d
taehee <dadac76@hotmail.com>
2021-08-17 00:18:31 -0700

    remove STATICFILES_DIRS STATIC_ROOT

--- lottocomboapi/settings.py
+++ lottocomboapi/settings.py
@@ -133,10 +133,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = BASE_DIR / 'static'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
