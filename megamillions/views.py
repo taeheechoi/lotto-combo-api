9d758643a257e7ba50ebe9a9541729f0d411ed09
taehee <dadac76@hotmail.com>
2021-08-18 15:35:36 -0700

    filter top_pick

--- megamillions/views.py
+++ megamillions/views.py
@@ -20,6 +20,9 @@ class WinningNumbersCombonationList(ListAPIView):
     def get_queryset(self):
         queryset = WinningNumbersCombination.objects.all().order_by('-winning_numbers_combination_occurrence')
         numbers = self.request.query_params.get('numbers')
+        top_pick = self.request.query_params.get('top-pick')
         if numbers is not None:
             queryset = queryset.filter(winning_numbers_combination__icontains=numbers).order_by('-winning_numbers_combination_occurrence')
+        if top_pick is not None:
+            queryset = queryset.filter(top_pick=top_pick).order_by('-winning_numbers_combination_occurrence')
         return queryset
