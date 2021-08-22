46a23049346223dd0f953c2347985d60336e64ee
taehee <dadac76@hotmail.com>
2021-08-22 00:28:11 -0700

    lowercase

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -11,12 +11,12 @@ from lottery.models import MegaMillions, Powerball, WinningNumbersCombination
 url = 'https://data.ny.gov/api/views/5xaw-6ayf/rows.json?accessType=DOWNLOAD'
 
 game_info = {
-    'MEGAMILLIONS': {
+    'megamillions': {
         'url': 'https://data.ny.gov/api/views/5xaw-6ayf/rows.json?accessType=DOWNLOAD',
         'model': MegaMillions,
  
     },
-    'POWERBALL': {
+    'powerball': {
         'url': 'https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD',
         'model': Powerball
     },
@@ -57,10 +57,10 @@ def winning_numbers_combinations(win_nums_list: list) -> Tuple[list, int]:
 def load_winning_numbers(game: str, model: models, win_nums_data: list) -> None:
     # 2013-04-30T00:00:00
 
-    if game == 'MEGAMILLIONS':
+    if game == 'megamillions':
         load_data = [model(draw_date=item[8], winning_numbers=item[9],
                                 ball=item[10], multiplier=item[-1], number_of_draws=len(win_nums_data)) for item in win_nums_data]
-    if game == 'POWERBALL':
+    if game == 'powerball':
         load_data = [model(draw_date=item[8], winning_numbers=item[9][:-2],
                                 ball=item[9][-2:], multiplier=item[-1], number_of_draws=len(win_nums_data)) for item in win_nums_data]
 
@@ -72,10 +72,10 @@ def load_winning_numbers_combinations(game: str, win_nums_data: list) -> None:
     top_occurrence_min_occurrence = 18 # about 50 records = pagination size. Get records once then will be shuffled to generate numbers
     min_occurrence = 6 # added due to limit of free version less than 10k rows
     
-    if game == 'MEGAMILLIONS':
+    if game == 'megamillions':
         win_nums_list = [  # ('01', '09', '17', '27', '34', '*24') * mega number
             (*item[9].split(' '), f'*{item[10]}') for item in win_nums_data]
-    if game == 'POWERBALL':
+    if game == 'powerball':
         win_nums_list = [  # ('01', '09', '17', '27', '34', '*24') * mega number
             (*item[9][:-2].split(' '), f'*{item[9][-2:]}') for item in win_nums_data]
 
