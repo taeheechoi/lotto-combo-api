50e64b48ae154ef39ad61288d9b2bb3133f8b68a
taehee <dadac76@hotmail.com>
2021-08-21 21:49:29 -0700

    + game filter

--- lottery/views.py
+++ lottery/views.py
@@ -1,8 +1,11 @@
-from lottery.serializers import MegaMillionsSerializer
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from .serializers import MegaMillionsSerializer, PowerballSerializer, WinningNumbersCombinationSerializer
+
+from lottery.serializers import MegaMillionsSerializer
+
 from .models import MegaMillions, Powerball, WinningNumbersCombination
+from .serializers import (MegaMillionsSerializer, PowerballSerializer,
+                          WinningNumbersCombinationSerializer)
 
 # Create your views here.
 
@@ -25,13 +28,14 @@ class WinningNumbersCombonationList(ListAPIView):
     def get_queryset(self):
         queryset = WinningNumbersCombination.objects.all().order_by('game',
                                                                     '-winning_numbers_combination_occurrence')
+        game = self.request.query_params.get('game')
         numbers = self.request.query_params.get('numbers')
         top_occurrence = self.request.query_params.get('top-occurrence')
 
         if numbers is not None:
-            queryset = queryset.filter(winning_numbers_combination__icontains=numbers).order_by('game',
+            queryset = queryset.filter(game=game, winning_numbers_combination__icontains=numbers).order_by('game',
                                                                                                 '-winning_numbers_combination_occurrence')
         if top_occurrence is not None:
-            queryset = queryset.filter(top_occurrence=top_occurrence).order_by('game',
+            queryset = queryset.filter(game=game, top_occurrence=top_occurrence).order_by('game',
                                                                                '-winning_numbers_combination_occurrence')
         return queryset
