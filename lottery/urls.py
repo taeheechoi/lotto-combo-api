0b18904b3ec269a86852a87ea4781d7a9bb40697
taehee <dadac76@hotmail.com>
2021-08-22 00:16:44 -0700

    url to match with frontend

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import MegaMillionsList, PowerballList, WinningNumbersCombonationList
 urlpatterns = [
-    path('mega-millions/', MegaMillionsList.as_view()),
+    path('megamillions/', MegaMillionsList.as_view()),
     path('powerball/', PowerballList.as_view()),
     path('winning-numbers-combinations/', WinningNumbersCombonationList.as_view()),
 
